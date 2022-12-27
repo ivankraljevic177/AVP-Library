f47a2ffd08ddfa8bfe287f7605be29335455c541
SpajicM <marinspajic1998@gmail.com>
2022-12-27 20:35:15 +0100

    Initial backend setup

---
+++ backend/jwt.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+function signJwt(user_id) {
+  const token = jwt.sign({ sub: user_id }, process.env.SECRET);
+  if (!token) return false;
+  return token;
+}
+
+function verifyJwt(req, res, next) {
+  const authorization = req.header("authorization");
+  const token = authorization ? authorization.split("Bearer ")[1] : undefined;
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded;
+  } catch {
+    return res.status(401).send("Invalid token");
+  }
+
+  return next();
+}
+
+module.exports = { signJwt, verifyJwt };
