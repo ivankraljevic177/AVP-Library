e0c9909400380c4f6b1033d70c99dcf11f6faeb0
Tomislav Bratic <ticbra1337@gmail.com>
2023-01-04 17:06:14 +0100

    --no-edit

--- frontend/src/components/BookList.js
+++ frontend/src/components/BookList.js
@@ -8,6 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
